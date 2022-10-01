779f19227c06406d4d5ff721d25a82221e639213
keltsjsx <kelts.jsx@gmail.com>
2022-10-02 06:59:40 +0700

    HTTP Post and JSON Binding

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -13,12 +14,13 @@ func main() {
 	router.GET("/hello", helloHandler)
 	router.GET("/books/:id", booksHandler)
 	router.GET("/movies", moviesHandler)
+	router.POST("/books", postBooksHandler)
 	router.Run()
 }
 
 func rootHandler(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{
-		"name":    "Fuad Muhammad Nur",
+		"name":    "Keltskaya",
 		"address": "Depok",
 	})
 }
@@ -38,4 +40,26 @@ func booksHandler(ctx *gin.Context) {
 func moviesHandler(ctx *gin.Context) {
 	title := ctx.Query("title")
 	ctx.JSON(http.StatusOK, gin.H{"title": title})
+}
+
+type BookInput struct {
+	Title    string
+	Price    int
+	SubTitle string `json:"sub_title"`
+}
+
+func postBooksHandler(ctx *gin.Context) {
+	var bookInput BookInput
+
+	err := ctx.ShouldBindJSON(&bookInput)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"title":     bookInput.Title,
+		"price":     bookInput.Price,
+		"sub_title": bookInput.SubTitle,
+	})
 }
